e77813d06db095b8ad4175140ece3a5a957c0d5a
Yashvar22 <yashvardhan.22scse1012863@galgotiasuniversity.edu.in>
2025-03-12 15:51:27 +0530

    comaprison

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -31,7 +31,43 @@
 //""==>false
 //"hites" ==>true;
 
-let someNumber = 33;
-let StringNum = String(someNumber);
-console.log(someNumber);
-console.log(typeof StringNum);
+// let someNumber = 33;
+// let StringNum = String(someNumber);
+// console.log(someNumber);
+// console.log(typeof StringNum);
+
+//*********************Operations*****************************
+
+let val = 3;
+let negVal = -val;
+// console.log(negVal);
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+// let str1 = "hello";
+// let str2 = " hitesh";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+//link to study
+//ahttps://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null == 0); //flase
+// console.log(null > 0); //flase
+// console.log(null >= 0); //true
+
+// console.log(undefined == 0);//false
+// console.log(undefined >0);//fasle
+// console.log(undefined < 0);//false
+
+// === strict check(value,datatye)
+
+console.log("2" === 2); //false
