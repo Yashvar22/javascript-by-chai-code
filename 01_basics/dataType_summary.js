4740ba4d496508e04b3cc9b40320fdf64c45c8ad
Yashvar22 <yashvardhan.22scse1012863@galgotiasuniversity.edu.in>
2025-03-17 23:54:30 +0530

    cHANGES

--- 01_basics/dataType_summary.js
+++ 01_basics/dataType_summary.js
@@ -40,5 +40,27 @@ const myFun = function () {
   console.log("Hello World");
 };
 
-console.log(typeof score);
-console.log(typeof Symbol);
+console.log(typeof myObj);
+console.log(typeof myFun);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack(Primitive) , Heap(Non-Primitive)
+
+let myYoutube = "hiteshChoudary";
+
+let anotherName = myYoutube;
+
+anotherName = "chaiaurcode";
+console.log(myYoutube);
+console.log(anotherName);
+
+let user1 = {
+  email: "user@gmail.com",
+  upi: "user@ybl",
+};
+
+let user2 = user1;
+user2.email = "hitesh@gmail.com";
+console.log(user1.email);
+console.log(user2.email);
