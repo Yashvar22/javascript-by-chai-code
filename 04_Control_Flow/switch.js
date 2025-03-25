fa3a4cc051d469e69013600ef8c8c6ced361d1e3
Yashvar22 <yashvardhan.22scse1012863@galgotiasuniversity.edu.in>
2025-03-26 00:51:36 +0530

    switch

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,31 @@
+//switch case
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march";
+
+switch (month) {
+  case "jan":
+    console.log("January");
+    break;
+  case "feb":
+    console.log("feb");
+    break;
+  case "march":
+    console.log("March");
+    break;
+  case "april":
+    console.log("April");
+    break;
+  default:
+    console.log("defdault case match");
+
+    break;
+}
