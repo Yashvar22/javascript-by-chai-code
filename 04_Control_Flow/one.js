701ea4cb51e30d7b47ed42bf73857b1d9a10351f
Yashvar22 <yashvardhan.22scse1012863@galgotiasuniversity.edu.in>
2025-03-26 00:44:43 +0530

    one

--- 04_Control_Flow/one.js
+++ 04_Control_Flow/one.js
@@ -20,10 +20,34 @@
 //2===2
 // <,>,<=,>=,==,!=,===(type),!==
 
-const score = 200;
-if (score > 100) {
-  var power = "fly";
-  console.log(`User power: ${power}`);
-}
+// const score = 200;
+// if (score > 100) {
+//   const power = "fly";
+//   console.log(`User power: ${power}`);
+// }
 
-console.log(`User power: ${power}`);
+// console.log(`User power: ${power}`);
+//const balance = 1000;
+// if (balance > 500) console.log("test"), console.log("test2");//bad practice
+
+// if (balance < 500) {
+//   console.log("less than 500");
+// } else if (balance < 750) {
+//   console.log("less than 750");
+// } else if (balance < 900) {
+//   console.log("less than 900"); // Fixed the incorrect text
+// } else if (balance < 1200) {
+//   // Added missing condition for 1000
+//   console.log("less than 1200");
+// }
+
+const userLoggesIN = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+if (userLoggesIN && debitCard && 2 == 2) {
+  console.log("Allow to buy course");
+}
+if (loggedInFromGoogle || loggedInFromEmail) {
+  console.log("User logged in");
+}
